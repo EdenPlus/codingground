af1a2c839e2b04f4c8d2131aa45c538a45537521
Eden <TGPforeva@Gmail.com>
2017-02-02 09:57:17 -0800

    Update main.py

--- PythonPrimesThatSumTo/main.py
+++ PythonPrimesThatSumTo/main.py
@@ -1,3 +1,29 @@
-# Hello World program in Python
-    
-print ("Hello World!");
+# Python3 program to find pairs of prime numbers that have a sum of a given number
+# Reproduction of a lost previous version (Coding ground has been glitchy)
+# Reproduction took ~30 minutes to write
+# Original took ~60 minutes to write
+
+# Clocking benchmark: 10000
+# Clocked (Original): ~3 seconds
+# Clocked (Reproduction): ~3 seconds
+
+def isPrimeNumber(x):
+    if x >= 2:
+        for y in range(2,x):
+            if not ( x % y ):
+                return False
+    else:
+	    return False
+    return True
+
+def primesThatSum(a):
+    print ("Please wait a moment...")
+    for b in range(a):
+        for c in range(b):
+            if b + c == a:
+                if isPrimeNumber(b):
+                    if isPrimeNumber(c):
+                        print (str(b) + " + " + str(c) + " = " + str(a))
+    print ("We are done")
+
+primesThatSum(10000)
